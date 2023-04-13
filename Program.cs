fd161f78adfb9032af3d9cb16a60c5ddcc059ac3
Dorian Gray's Heiress <62262433+FED-Polina@users.noreply.github.com>
2023-04-13 18:47:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        string commonDigits = GetCommonDigits(firstNumber, secondNumber);
+    }
+
+    static string GetCommonDigits(int num1, int num2)
+    {
+        string strNum1 = num1.ToString();
+        string strNum2 = num2.ToString();
+        string commonDigits = "";
+
+        foreach (char digit in strNum1)
+        {
+            if (strNum2.Contains(digit))
+            {
+                if (!commonDigits.Contains(digit))
+                {
+                    commonDigits += digit;
+                }
+            }
+
+        }
+        if (commonDigits.Length == 0)
+        {
+            Console.WriteLine("No common digits found"); 
+        }
+        else
+        {
+            Console.WriteLine("Digits common in both numbers: " + commonDigits);
+        }
+
+        return commonDigits;
+    }
+}
